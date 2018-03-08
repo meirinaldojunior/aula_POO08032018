a0a596d99d8d0b408851045ac55ec1d1738e26d6
Meirinaldo Júnior <meirinaldojunior@users.noreply.github.com>
2018-03-08 19:42:34 -0300

    git init

---
+++ src/aula_poo08032018/Aula_POO08032018.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula_poo08032018;
+
+/**
+ *
+ * @author meirinaldojunior
+ */
+public class Aula_POO08032018 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/aula_poo08032018/Msg.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula_poo08032018;
+
+/**
+ *
+ * @author meirinaldojunior
+ */
+public class Msg {
+ 
+    public static String prompt(String x){   
+        return x;
+    }
+    
+    public static void mostra(String x){
+        
+    }  
+    
+    public static void texto (String x){
+        
+    }
+    
+    public static int pedeNumero(String x){
+        return 0;
+    }
+}
